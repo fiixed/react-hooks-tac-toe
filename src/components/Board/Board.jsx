a3626c636ec22e7365ac70ec92008f40d59480fd
fiixed <fiixed@gmail.com>
2021-01-23 17:18:15 -0500

    winning combination highlight

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -3,18 +3,21 @@ import { Cell } from '../Cell/Cell'
 import './Board.css'
 
 export const Board = () => {
+    const cellValues = ['X', 'X', 'X', 'O', 'O', 'X', 'O', '', ''];
+    const winningCombination = [0, 1, 2];
+    const cells = cellValues.map((value, index) => {
+        const canHighlight = winningCombination && 
+                            winningCombination.indexOf(index) >= 0;
+
+        return <Cell 
+                key={index} 
+                value={value} 
+                canHighlight={canHighlight}/>
+    });
     return (
         <>
         <div id="board">
-            <Cell value="X" canHighlight={true}/>
-            <Cell value="X" canHighlight={true}/>
-            <Cell value="X" canHighlight={true}/>
-            <Cell value="X" canHighlight={false}/>
-            <Cell value="X" canHighlight={false}/>
-            <Cell value="X" canHighlight={false}/>
-            <Cell value="X" canHighlight={false}/>
-            <Cell value="X" canHighlight={false}/>
-            <Cell value="X" canHighlight={false}/>
+            {cells}
         </div>
     </>
     )
