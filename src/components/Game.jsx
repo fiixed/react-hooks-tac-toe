c253d2a10c44244d6c4aaee9a9b655e4b0a6e10c
fiixed <fiixed@gmail.com>
2021-01-26 15:56:39 -0500

    start new game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,15 +4,30 @@ import { Board } from './Board/Board';
 import ResultModal from './ResultModal/ResultModal';
 import { calculateWinner } from '../utils/WinnerCalculator';
 
+const initCellValues = ['', '', '', '', '', '', '', '', ''];
+const initXIsNext = true;
+const initIsGameOver = false;
+const initNumberOfTurnsLeft = 9;
+const initWinningCombination = [];
+
 export const Game = () => {
-    const [cellValues, setCellValues] = useState(['', '', '', '', '', '', '', '', '']);
-    const [xIsNext, setXIsNext] = useState(true);
-    const [isGameOver, setIsGameOver] = useState(false);
-    const [numberOfTurnsLeft, setNumberOfTurnsLeft] = useState(9);
+    const [cellValues, setCellValues] = useState(initCellValues);
+    const [xIsNext, setXIsNext] = useState(initXIsNext);
+    const [isGameOver, setIsGameOver] = useState(initIsGameOver);
+    const [numberOfTurnsLeft, setNumberOfTurnsLeft] = useState(initNumberOfTurnsLeft);
     const [winner, setWinner] = useState();
-    const winningCombination = [];
+    const [winningCombination, setWinningCombination] = useState(initWinningCombination);
 
     const isCellEmpty = (cellIndex) => cellValues[cellIndex] === '';
+
+    const restartGame = () => {
+        setCellValues(initCellValues);
+        setXIsNext(initXIsNext);
+        setIsGameOver(initIsGameOver);
+        setNumberOfTurnsLeft(initNumberOfTurnsLeft);
+        setWinner();
+        setWinningCombination(initWinningCombination);
+    };
 
     const onCellClicked = (cellIndex) => {
       if (isCellEmpty(cellIndex)) {
@@ -29,6 +44,7 @@ export const Game = () => {
         setIsGameOver(calcResult.hasResult);
         setNumberOfTurnsLeft(newNumberOfTurnsLeft);
         setWinner(calcResult.winner);
+        setWinningCombination(calcResult.winningCombination);
       }
       
   }
@@ -45,6 +61,7 @@ export const Game = () => {
     <ResultModal 
       isGameOver={isGameOver}
       winner={winner}
+      onNewGameClicked={restartGame}
     />
   </>
   );
